9c739913d5d66e9c3207b058574012c3dea8494e
Mats <mats@mats.sh>
2021-10-05 17:51:03 +0200

    Avoid leaking memory when setting the rich presence

--- mist/src/subprocess/friends.rs
+++ mist/src/subprocess/friends.rs
@@ -13,16 +13,19 @@ impl MistServiceFriends for MistServerService {
     fn set_rich_presence(&mut self, key: String, value: Option<String>) -> bool {
         // Turn the string into a c null terminated string
         let c_key = CString::new(key).unwrap_or_default();
+
+        static mut PRESENCE_VALUE: Option<CString> = None;
         // value can be None (NULL) to clear it
-        let c_value = value.map(|val| CString::new(val).ok()).flatten();
+        unsafe { PRESENCE_VALUE = value.map(|val| CString::new(val).ok()).flatten() }
 
         unsafe {
             steamworks_sys::SteamAPI_ISteamFriends_SetRichPresence(
                 self.steam_friends,
                 c_key.as_ptr() as *const _,
                 // Get the ptr to the str if it has a value, otherwise return null
-                c_value
-                    .map(|v| v.into_raw() as *const _)
+                PRESENCE_VALUE
+                    .as_ref()
+                    .map(|v| v.as_ptr() as *const _)
                     .unwrap_or(std::ptr::null()),
             )
         }
